79d17628277f543da590ceddf631ec643608a414
Melissa Autumn <melissa@thunderbird.net>
2022-11-23 14:56:58 -0800

    Some suggested changes, and an upgrade to Python 3

--- build_calendar.py
+++ build_calendar.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-import datetime
+import sys
+from datetime import datetime, timedelta
 import json
+import os
 import time
-
+import icalendar
 import requests
-import settings_private
 
 # This will correspond with specific api locale sets
 LOCALES = [
@@ -12,70 +13,32 @@ LOCALES = [
     'US'
 ]
 
-# General exception
-class APIException(Exception):
-    def __init__(self, status_code):
-        self.status_code = status_code
-
 class CountryNotSupportedException(Exception):
     def __init__(self, country):
         self.country = country
 
-class RateLimitException(Exception):
-    pass
-class AuthException(Exception):
-    pass
-
 API_URL = "https://calendarific.com/api/v2/holidays"
-
-def quick_cache_put(data, locale, year):
-    fh = open('api_cache_%s_%s.txt' % (locale, year), 'w')
-    fh.write(json.dumps(data))
-    fh.close()
-
-def quick_cache_get(locale, year):
-    try:
-        fh = open('api_cache_%s_%s.txt' % (locale, year), 'r')
-        data = fh.read()
-        fh.close()
-    except IOError:
-        return None
-
-    return json.loads(data)
 
 def query_calendarific(api_key, country, year, calendar_type):
     if country not in LOCALES:
         raise CountryNotSupportedException(country)
 
-    cached_json = quick_cache_get(country, year)
-    if cached_json is not None:
-        return cached_json['response']['holidays']
+    payload = {
+        'api_key': api_key,
+        'country': country,
+        'year': year,
+        'type': calendar_type
+    }
 
-    url = [
-        API_URL,
-        '?',
-        'api_key=%s' % api_key,
-        'country=%s' % country,
-        'year=%s' % year,
-        'type=%s' % calendar_type
-    ]
-    url = "&".join(x for x in url)
+    response = requests.get(API_URL, params=payload)
 
-    #print("Requesting: ", url)
+    response.raise_for_status()
 
-    response = requests.get(url)
-
-    # We only really care about auth and rate limit, so fall back to the generic api exception if we're hit with anything else
-    if response.status_code == 401:
-        raise AuthException()
-    if response.status_code == 429:
-        raise RateLimitException()
-    elif response.status_code != 200:
-        raise APIException(response.status_code)
-
-    quick_cache_put(response.json(), country, year)
-
-    return response.json()['response']['holidays']
+    try:
+        return response.json()['response']['holidays']
+    except KeyError:
+        print("Bad response from {}, {}, {}".format(country, year, calendar_type))
+        return None
 
 
 # Lifted from calendarific api - May not be needed
@@ -92,7 +55,7 @@ class CalendarTransformer(object):
         self.unique_id = 0
         self.name = ''
         self.description = ''
-        self.iso_date = self.iso_date_to_date_obj('1970-01-01')
+        self.iso_date = datetime(1970, 1, 1)
         self.calendar_type = ''
         self.year = year
 
@@ -100,47 +63,29 @@ class CalendarTransformer(object):
         if data:
             self.from_api(data)
 
-
-    # TODO: See if there's already a date utils for python 2
-    @staticmethod
-    def iso_date_to_date_obj(date):
-        date_parts = date.split('-')
-        return datetime.datetime(int(date_parts[0]), int(date_parts[1]), int(date_parts[2]))
-
-    @staticmethod
-    def date_to_caldate(date):
-        return date.strftime('%Y%m%dT%H%M%SZ')
-
-    @staticmethod
-    def date_to_isodate(date):
-        return date.strftime('%Y%m%d')
-
     def from_api(self, data):
         raise NotImplementedError()
 
     def to_ics(self):
-        # FIXME: Quite messy - use icalendar package instead
-        return """BEGIN:VEVENT
-UID:%s
-LAST-MODIFIED:%s
-DTSTART;VALUE=DATE:%s
-DTEND;VALUE=DATE:%s
-SUMMARY:%s
-DESCRIPTION:%s
-DTSTAMP:%s
-CLASS:PUBLIC
-TRANSP:TRANSPARENT
-CATEGORIES:Holidays
-END:VEVENT
-""" % (
-            "%s-%s" % (self.unique_id, self.year),
-            self.date_to_caldate(datetime.datetime.now()),
-            self.date_to_isodate(self.iso_date),
-            self.date_to_isodate(self.iso_date + datetime.timedelta(days=1)),
-            self.name,
-            self.description[0],
-            self.date_to_caldate(self.iso_date)
-        )
+        ievt = icalendar.Event()
+
+        data = {
+            'uid': "{}-{}".format(self.unique_id, self.year),
+            'last-modified': datetime.now(),
+            'dtstart': self.iso_date,
+            'dtend': self.iso_date + timedelta(days=1),
+            'summary': self.name,
+            'description': self.description,
+            'dtstamp': datetime.now(),
+            'class': 'public',
+            'transp': 'opaque' if self.calendar_type == CALENDAR_TYPES['NATIONAL'] else 'transparent',
+            'categories': 'Holidays',
+        }
+
+        for key, value in data.items():
+            ievt.add(key, value)
+
+        return ievt
 
 
 # See: https://calendarific.com/api-documentation
@@ -153,7 +98,7 @@ class CalendarificTransformer(CalendarTransformer):
         self.unique_id = data['urlid']
         self.name = data['name']
         self.description = data['description'],
-        self.iso_date = self.iso_date_to_date_obj(data['date']['iso'])
+        self.iso_date = datetime.fromisoformat(data['date']['iso'])
 
         return self
 
@@ -162,31 +107,58 @@ class HolidayAPITransformer(CalendarTransformer):
     def from_api(self, data):
         self.unique_id = data['uuid']
         self.name = data['name']
-        self.iso_date = self.iso_date_to_date_obj(data['date'])
+        self.iso_date = datetime.fromisoformat(data['date'])
         self.calendar_type = CALENDAR_TYPES['NATIONAL'] if bool(data['public']) else CALENDAR_TYPES['OBSERVANCE']
 
 def build_calendars():
+
+    try:
+        api_key = os.environ['CALENDARIFIC_API_KEY']
+    except KeyError:
+        sys.exit("No `CALENDARIFIC_API_KEY` defined.")
+
+    try:
+        api_tier = os.environ['os.CALENDARIFIC_API_TIER']
+    except KeyError:
+        api_tier = 'free'
+
+    years_to_generate = 1 # 3
+    current_year = datetime.now().year
+
     for locale in LOCALES:
-        fh = open('%sHolidays.ics' % locale, 'w')
+        ical = icalendar.Calendar()
+        ical.add('prodid', '-//Mozilla.org/NONSGML Mozilla Calendar V1.1//EN')
+        ical.add('version', '2.0')
 
-        fh.write("""BEGIN:VCALENDAR
-PRODID:-//Mozilla.org/NONSGML Mozilla Calendar V1.1//EN
-VERSION:2.0
-""")
+        for i in range(0, years_to_generate):
+            year = current_year + i
+            try:
+                holidays = query_calendarific(api_key, locale, year, CALENDAR_TYPES['NATIONAL'])
+                formatted_holidays = [CalendarificTransformer(holiday, year, CALENDAR_TYPES['NATIONAL']) for holiday
+                                      in holidays]
 
-        years_to_generate = [2022, 2023]
+                for holiday in formatted_holidays:
+                    ical.add_component(holiday.to_ics())
 
-        for year in years_to_generate:
-            holidays = query_calendarific(settings_private.CALENDARIFIC_API_KEY, locale, year, CALENDAR_TYPES['NATIONAL'])
-            formatted_holidays = [ CalendarificTransformer(holiday, year, CALENDAR_TYPES['NATIONAL']) for holiday in holidays  ]
-            ics_files = [ holiday.to_ics() for holiday in formatted_holidays ]
+            except requests.HTTPError as err:
+                match err.response.status_code:
+                    # API limit reached
+                    case requests.status_codes.codes.too_many_requests:
+                        print("API limit reached, breaking from loop")
+                        break
+                    # Bad API key
+                    case requests.status_codes.codes.unauthorized:
+                        print("Bad or missing API key")
+                        break
+                    case _:
+                        print("Error {}".format(err.response.status_code))
+                        continue
 
-            for ics in ics_files:
-                fh.write(ics)
-
-            # Wait 1 second due to api restrictions
+        # Wait 1 second due to free api restrictions
+        if api_tier == 'free':
             time.sleep(1)
 
-        fh.write('END:VCALENDAR')
+        with open('media/caldata/autogen/{}Holidays.ics'.format(locale), 'wb') as fh:
+            fh.write(ical.to_ical())
 
-        fh.close()
+build_calendars()
